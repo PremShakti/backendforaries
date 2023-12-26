84cb47b6b51a196956b1c53e06cf9eacae7f1496
PremShakti <premshakti.ps@gmail.com>
2023-12-26 09:08:07 +0530

    a

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const express=require('express')
-require('dotenv').config()
-const {mainrouter}=require('./Routes/Routes')
-const {connect}=require('./db')
-const cors=require('cors')
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose')
-let app=express()
-let PORT=process.env.PORT||3000
-app.use(express.json())
-app.use(cors())
+const express = require("express");
+require("dotenv").config();
+const { mainrouter } = require("./Routes/Routes");
+const { connect } = require("./db");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+let app = express();
+let PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
-
 
 const connectDB = async () => {
   try {
@@ -20,39 +19,24 @@ const connectDB = async () => {
     console.log(error);
     process.exit(1);
   }
-}
+};
 
+app.use("/", mainrouter);
 
-
-app.use("/",mainrouter)
-
-
-
-
-
-
-
-
-app.all('*', (req,res) => {
-    res.json({"every thing":"is awesome"})
-})
+app.all("*", (req, res) => {
+  res.json({ "every thing": "is awesome" });
+});
 
 //Connect to the database before listening
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("listening for requests");
-    })
-})
-
-
-
-
-
-
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
+});
 
 // app.listen(PORT, async()=>{
 //     try {
-       
+
 //         console.log(`your port is running in ${PORT}`)
 //     } catch (error) {
 //         console.log(error)
